22da5f1088e6194497f115956be74cb7adb9bb95
Emil Teurnberg <e.teurnberg@gmail.com>
2019-11-13 14:48:28 +0100

    Revamps installation script
    * Now installs oh-my-zsh if not already present
    * Adds a check before installing the oh-my-zsh theme
    * Now defines all dotfiles to be tracked in an array, and loops over
    this array when soft linking them to the home directory

--- install.sh
+++ install.sh
@@ -5,9 +5,20 @@ export DOTFILES_DIRECTORY
 # Variables, make any wanted changes here
 VUNDLE_LOCATION=~/.vim/bundle/Vundle.vim
 TPM_LOCATION=~/.tmux/plugins/tpm
+OH_MY_ZSH_LOCATION=~/.oh-my-zsh
+OH_MY_ZSH_POWERLEVEL9K_THEME_LOCATION="$OH_MY_ZSH_LOCATION/custom/themes/powerlevel9k"
+FONTS_DIR=/fonts
 
-# Get absolute path to the directory the script is run in
+# Get absolute path to the directory the script is located in
 DOTFILES_DIRECTORY="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# List of dotfiles being kept track of
+dotfiles=(".vimrc" ".tmux.conf" ".tmuxline_snapshot.conf" ".zshrc" ".gitconfig" ".gitignore_global")
+
+# Install oh-my-zsh, if not installed already
+if [ ! -d "$OH_MY_ZSH_LOCATION" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # Install Vundle for vim, if not installed already
 if [ ! -d "$VUNDLE_LOCATION" ]; then
@@ -19,9 +30,10 @@ if [ ! -d "$TPM_LOCATION" ]; then
     git clone https://github.com/tmux-plugins/tpm $TPM_LOCATION
 fi
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-FONTS_DIR=/fonts
+# Install oh-my-zsh theme powerlevel9k if not already installed
+if [ ! -d "$OH_MY_ZSH_POWERLEVEL9K_THEME_LOCATION" ]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
 
 # Install powerline fonts
 if [ ! -d "$DOTFILES_DIRECTORY$FONTS_DIR" ]; then
@@ -29,12 +41,9 @@ if [ ! -d "$DOTFILES_DIRECTORY$FONTS_DIR" ]; then
     ./fonts/install.sh
 fi
 
-ln -sfv "$DOTFILES_DIRECTORY/.vimrc" ~
-ln -sfv "$DOTFILES_DIRECTORY/.tmux.conf" ~
-ln -sfv "$DOTFILES_DIRECTORY/.tmuxline_snapshot.conf" ~
-ln -sfv "$DOTFILES_DIRECTORY/.zshrc" ~
-ln -sfv "$DOTFILES_DIRECTORY/.gitconfig" ~
-ln -sfv "$DOTFILES_DIRECTORY/.gitignore_global" ~
+for dotfile in "${dotfiles[@]}"; do
+  ln -sfv "${HOME}/${dotfile}" "$DOTFILES_DIRECTORY"
+done
 
 # Config git to use new global gitignore file
 git config --global core.excludesfile ~/.gitignore_global
